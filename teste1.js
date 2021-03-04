a2ed9af153efd6ad6346aec8ab57ef05ab05e7a6
gilbertocarvalho <gilberto.soua2013@gmail.com>
2021-03-04 13:45:32 -0300

    alguns comandos do javascript

---
+++ teste1.js
@@ -0,0 +1,52 @@
+let  nome = "VLT de Santos";
+
+//variável aceita string
+
+// let = variável local
+
+// var = variável global 
+
+// const = constante local
+
+let n1 = 10;
+
+//variável aceita numero inteiro
+
+let n2 = 4.2;
+
+//variável aceita número racional
+
+let b = false;
+
+//variável aceita boolean
+
+let aluno = {ra:"0001" , nome : "joão silva" , rg: "333333333"};
+
+//variável aceita objeto
+
+let array = [3,3,3,4,4];
+
+//variável aceita vetor
+let x1 = null; 
+
+//variável aceita valor nulo
+let a  = 1;
+let c= '1';  
+
+if(a === c){
+
+console.log("são iguais");
+
+}else{
+
+console.log("não são iguais");
+
+}
+
+//console.log é a sintáxe para a impressão de um valor no console
+
+console.log(x1);
+
+console.log(nome);  
+
+
